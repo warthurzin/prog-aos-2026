6c4482de887ca590e57deae47933010ee4af0b45
Arthur  Silveira <arthurmarques2148@gmail.com>
2026-02-26 22:58:50 +0000

    adicionando express + get / + utilização de variável PORT

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -1,4 +1,19 @@
 import "dotenv/config";
+import express from "express";
 
-console.log("Hello World!!!");
-console.log(process.env.MESSAGE);
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Servidor rodando!\n" + process.env.MESSAGE);
+});
+
+const port = process.env.PORT ?? 3000;
+
+app.listen(port, () =>
+  console.log(
+    "Aplicativo de exemplo escutando na porta " +
+      port +
+      "\n" +
+      process.env.MESSAGE,
+  ),
+);
